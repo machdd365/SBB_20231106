a1aefff9936f778eb25bafd761cd6675fc8aa846
kjk <jkk1637@naver.com>
2023-11-06 17:58:50 +0900

    새프로젝트

---
+++ src/test/java/com/mysite/sbb/Sbb20231106ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb20231106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
