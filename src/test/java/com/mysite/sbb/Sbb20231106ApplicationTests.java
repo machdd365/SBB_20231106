6dedc3b5738ae516a5037000366f38b390ce2774
kjk <jkk1637@naver.com>
2023-11-08 17:59:42 +0900

    Test code 추가

--- src/test/java/com/mysite/sbb/Sbb20231106ApplicationTests.java
+++ src/test/java/com/mysite/sbb/Sbb20231106ApplicationTests.java
@@ -1,10 +1,63 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Sbb20231106ApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Test
+	void testQuestionRepositorySave(){
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1); //첫번째 질문 저장
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2); //두번째 질문 저장
+	}
+
+	@Test
+	void testQuestionRepositoryRead(){
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(4, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	void testQuestionRepositoryReadFindByID(){
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if (oq.isPresent()){
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+
+	@Test
+	void testQuestionRepositoryReadFindBySubject(){
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		System.out.println(q.getId());
+		//assertEquals(1, q.getId());
+	}
+
+
+
 
 	@Test
 	void contextLoads() {
