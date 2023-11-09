b3a5314769e3d0f8b5c5eb297ca9ecfc54418df3
kjk <jkk1637@naver.com>
2023-11-09 11:09:01 +0900

    Test code 추가

--- src/test/java/com/mysite/sbb/Sbb20231106ApplicationTests.java
+++ src/test/java/com/mysite/sbb/Sbb20231106ApplicationTests.java
@@ -3,18 +3,23 @@ package com.mysite.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class Sbb20231106ApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+
+	@Autowired
+	private AnswerRepository answerRepository;
 
 	@Test
 	void testQuestionRepositorySave(){
@@ -54,6 +59,76 @@ class Sbb20231106ApplicationTests {
 		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
 		System.out.println(q.getId());
 		//assertEquals(1, q.getId());
+	}
+
+	@Test
+	void testQuestionRepositoryReadfindBySubjectAndContent(){
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다."
+		);
+		assertEquals(1, q.getId());
+	}
+
+	@Test
+	void testQuestionRepositoryReadfindBySubjectLike() {
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	void testQuestionRepositoryUpdateFindById(){
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
+	}
+
+	@Test
+	void testQuestionRepositoryDeleteFindById(){
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
+
+
+
+	@Test
+	void testAnswerRepositorySave(){
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q); //어떤질문의 답변인지 알려주기 위한 질문 객체
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
+	}
+
+	@Test
+	void testAnswerRepositoryFindByID(){
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
+	}
+
+	@Transactional //Test용 DB연결 유지(실제 서버에서는 DB연결이 계속 유지된다)
+	@Test
+	void testQuestionRepositoryFindAnswerToQuestion(){
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 
 
